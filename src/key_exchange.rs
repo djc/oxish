56deb20c9fdd9b19ef63fe944c3701ee9fc672d7
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-01-16 17:53:08 +0100

    Split into multiple files

---
+++ src/key_exchange.rs
@@ -0,0 +1,377 @@
+use std::str;
+
+use aws_lc_rs::rand;
+use tracing::debug;
+
+use crate::{
+    proto::{Decode, Decoded, Encode, MessageType, Packet, StreamState},
+    Error,
+};
+
+pub(crate) struct KeyExchange;
+
+impl<'a> StreamState<'a> for KeyExchange {
+    type Input = KeyExchangeInit<'a>;
+    type Output = KeyExchangeInit<'a>;
+}
+
+#[derive(Debug)]
+pub(crate) struct KeyExchangeInit<'a> {
+    cookie: [u8; 16],
+    key_exchange_algorithms: Vec<KeyExchangeAlgorithm<'a>>,
+    server_host_key_algorithms: Vec<PublicKeyAlgorithm<'a>>,
+    encryption_algorithms_client_to_server: Vec<EncryptionAlgorithm<'a>>,
+    encryption_algorithms_server_to_client: Vec<EncryptionAlgorithm<'a>>,
+    mac_algorithms_client_to_server: Vec<MacAlgorithm<'a>>,
+    mac_algorithms_server_to_client: Vec<MacAlgorithm<'a>>,
+    compression_algorithms_client_to_server: Vec<CompressionAlgorithm<'a>>,
+    compression_algorithms_server_to_client: Vec<CompressionAlgorithm<'a>>,
+    languages_client_to_server: Vec<Language<'a>>,
+    languages_server_to_client: Vec<Language<'a>>,
+    first_kex_packet_follows: bool,
+    extended: u32,
+}
+
+impl KeyExchangeInit<'static> {
+    pub(crate) fn new() -> Result<Self, Error> {
+        let mut cookie = [0; 16];
+        if rand::fill(&mut cookie).is_err() {
+            return Err(Error::FailedRandomBytes);
+        };
+
+        Ok(Self {
+            cookie,
+            key_exchange_algorithms: vec![KeyExchangeAlgorithm::Curve25519Sha256],
+            server_host_key_algorithms: vec![PublicKeyAlgorithm::Ed25519],
+            encryption_algorithms_client_to_server: vec![EncryptionAlgorithm::Aes128Ctr],
+            encryption_algorithms_server_to_client: vec![EncryptionAlgorithm::Aes128Ctr],
+            mac_algorithms_client_to_server: vec![MacAlgorithm::HmacSha2256],
+            mac_algorithms_server_to_client: vec![MacAlgorithm::HmacSha2256],
+            compression_algorithms_client_to_server: vec![CompressionAlgorithm::None],
+            compression_algorithms_server_to_client: vec![CompressionAlgorithm::None],
+            languages_client_to_server: vec![],
+            languages_server_to_client: vec![],
+            first_kex_packet_follows: false,
+            extended: 0,
+        })
+    }
+}
+
+impl<'a> Decode<'a> for KeyExchangeInit<'a> {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error> {
+        let Decoded {
+            value: packet,
+            next: next_packet,
+        } = Packet::decode(bytes)?;
+
+        let Decoded {
+            value: r#type,
+            next,
+        } = MessageType::decode(packet.payload)?;
+        if r#type != MessageType::KeyExchangeInit {
+            return Err(Error::InvalidPacket("unexpected message type"));
+        }
+
+        let Decoded {
+            value: cookie,
+            next,
+        } = <[u8; 16]>::decode(next)?;
+
+        let Decoded {
+            value: key_exchange_algorithms,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: server_host_key_algorithms,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: encryption_algorithms_client_to_server,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: encryption_algorithms_server_to_client,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: mac_algorithms_client_to_server,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: mac_algorithms_server_to_client,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: compression_algorithms_client_to_server,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: compression_algorithms_server_to_client,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: languages_client_to_server,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: languages_server_to_client,
+            next,
+        } = Vec::decode(next)?;
+
+        let Decoded {
+            value: first_kex_packet_follows,
+            next,
+        } = u8::decode(next)?;
+
+        let Decoded {
+            value: extended,
+            next,
+        } = u32::decode(next)?;
+
+        let value = Self {
+            cookie,
+            key_exchange_algorithms,
+            server_host_key_algorithms,
+            encryption_algorithms_client_to_server,
+            encryption_algorithms_server_to_client,
+            mac_algorithms_client_to_server,
+            mac_algorithms_server_to_client,
+            compression_algorithms_client_to_server,
+            compression_algorithms_server_to_client,
+            languages_client_to_server,
+            languages_server_to_client,
+            first_kex_packet_follows: first_kex_packet_follows != 0,
+            extended,
+        };
+
+        if !next.is_empty() {
+            debug!(bytes = ?next, "unexpected trailing bytes");
+            return Err(Error::InvalidPacket("unexpected trailing bytes"));
+        }
+
+        Ok(Decoded {
+            value,
+            next: next_packet,
+        })
+    }
+}
+
+impl Encode for KeyExchangeInit<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        MessageType::KeyExchangeInit.encode(buf);
+        buf.extend_from_slice(&self.cookie);
+        self.key_exchange_algorithms.encode(buf);
+        self.server_host_key_algorithms.encode(buf);
+        self.encryption_algorithms_client_to_server.encode(buf);
+        self.encryption_algorithms_server_to_client.encode(buf);
+        self.mac_algorithms_client_to_server.encode(buf);
+        self.mac_algorithms_server_to_client.encode(buf);
+        self.compression_algorithms_client_to_server.encode(buf);
+        self.compression_algorithms_server_to_client.encode(buf);
+        self.languages_client_to_server.encode(buf);
+        self.languages_server_to_client.encode(buf);
+        buf.push(if self.first_kex_packet_follows { 1 } else { 0 });
+        buf.extend_from_slice(&self.extended.to_be_bytes());
+    }
+}
+
+impl<T: Encode> Encode for [T] {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        let offset = buf.len();
+        buf.extend_from_slice(&[0, 0, 0, 0]);
+        let mut first = true;
+        for name in self {
+            match first {
+                true => first = false,
+                false => buf.push(b','),
+            }
+
+            name.encode(buf);
+        }
+
+        let len = (buf.len() - offset - 4) as u32;
+        if let Some(slice) = buf.get_mut(offset..offset + 4) {
+            slice.copy_from_slice(&len.to_be_bytes());
+        }
+    }
+}
+
+impl<'a, T: From<&'a str>> Decode<'a> for Vec<T> {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error> {
+        let Decoded { value: len, next } = u32::decode(bytes)?;
+
+        let Some(list) = next.get(..len as usize) else {
+            return Err(Error::Incomplete(Some(len as usize - next.len())));
+        };
+
+        let Some(next) = next.get(len as usize..) else {
+            return Err(Error::Unreachable("unable to extract rest after name list"));
+        };
+
+        let mut value = Self::new();
+        if list.is_empty() {
+            return Ok(Decoded { value, next });
+        }
+
+        for name in list.split(|&b| b == b',') {
+            match str::from_utf8(name) {
+                Ok(name) => value.push(T::from(name)),
+                Err(_) => return Err(Error::InvalidPacket("invalid name")),
+            }
+        }
+
+        Ok(Decoded { value, next })
+    }
+}
+
+#[derive(Debug)]
+enum KeyExchangeAlgorithm<'a> {
+    /// curve25519-sha256 (<https://www.rfc-editor.org/rfc/rfc8731>)
+    Curve25519Sha256,
+    Unknown(&'a str),
+}
+
+impl Encode for KeyExchangeAlgorithm<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::Curve25519Sha256 => buf.extend_from_slice(b"curve25519-sha256"),
+            Self::Unknown(name) => buf.extend_from_slice(name.as_bytes()),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for KeyExchangeAlgorithm<'a> {
+    fn from(value: &'a str) -> Self {
+        match value {
+            "curve25519-sha256" => Self::Curve25519Sha256,
+            _ => Self::Unknown(value),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum PublicKeyAlgorithm<'a> {
+    /// sh-ed25519 (<https://www.rfc-editor.org/rfc/rfc8709>)
+    Ed25519,
+    Unknown(&'a str),
+}
+
+impl Encode for PublicKeyAlgorithm<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::Ed25519 => buf.extend_from_slice(b"ssh-ed25519"),
+            Self::Unknown(name) => buf.extend_from_slice(name.as_bytes()),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for PublicKeyAlgorithm<'a> {
+    fn from(value: &'a str) -> Self {
+        match value {
+            "ssh-ed25519" => Self::Ed25519,
+            _ => Self::Unknown(value),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum EncryptionAlgorithm<'a> {
+    /// aes128-ctr (<https://www.rfc-editor.org/rfc/rfc4344#section-4>)
+    Aes128Ctr,
+    Unknown(&'a str),
+}
+
+impl Encode for EncryptionAlgorithm<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::Aes128Ctr => buf.extend_from_slice(b"aes128-ctr"),
+            Self::Unknown(name) => buf.extend_from_slice(name.as_bytes()),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for EncryptionAlgorithm<'a> {
+    fn from(value: &'a str) -> Self {
+        match value {
+            "aes128-ctr" => Self::Aes128Ctr,
+            _ => Self::Unknown(value),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum MacAlgorithm<'a> {
+    /// hmac-sha2-256 (<https://www.rfc-editor.org/rfc/rfc6668#section-2>)
+    HmacSha2256,
+    Unknown(&'a str),
+}
+
+impl Encode for MacAlgorithm<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::HmacSha2256 => buf.extend_from_slice(b"hmac-sha2-256"),
+            Self::Unknown(name) => buf.extend_from_slice(name.as_bytes()),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for MacAlgorithm<'a> {
+    fn from(value: &'a str) -> Self {
+        match value {
+            "hmac-sha2-256" => Self::HmacSha2256,
+            _ => Self::Unknown(value),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum CompressionAlgorithm<'a> {
+    None,
+    Unknown(&'a str),
+}
+
+impl Encode for CompressionAlgorithm<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::None => buf.extend_from_slice(b"none"),
+            Self::Unknown(name) => buf.extend_from_slice(name.as_bytes()),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for CompressionAlgorithm<'a> {
+    fn from(value: &'a str) -> Self {
+        match value {
+            "none" => Self::None,
+            _ => Self::Unknown(value),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Language<'a> {
+    Unknown(&'a str),
+}
+
+impl Encode for Language<'_> {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::Unknown(name) => buf.extend_from_slice(name.as_bytes()),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for Language<'a> {
+    fn from(value: &'a str) -> Self {
+        Self::Unknown(value)
+    }
+}
---
+++ src/proto.rs
@@ -0,0 +1,263 @@
+use std::{future::Future, iter};
+
+use aws_lc_rs::rand;
+use tokio::{io::AsyncReadExt, net::TcpStream};
+use tracing::debug;
+
+use crate::Error;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub(crate) enum MessageType {
+    Disconnect,
+    Ignore,
+    Unimplemented,
+    Debug,
+    ServiceRequest,
+    ServiceAccept,
+    KeyExchangeInit,
+    NewKeys,
+    Unknown(u8),
+}
+
+impl Encode for MessageType {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        match self {
+            Self::Disconnect => buf.push(1),
+            Self::Ignore => buf.push(2),
+            Self::Unimplemented => buf.push(3),
+            Self::Debug => buf.push(4),
+            Self::ServiceRequest => buf.push(5),
+            Self::ServiceAccept => buf.push(6),
+            Self::KeyExchangeInit => buf.push(20),
+            Self::NewKeys => buf.push(21),
+            Self::Unknown(value) => buf.push(*value),
+        }
+    }
+}
+
+impl<'a> Decode<'a> for MessageType {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error> {
+        let Decoded { value, next } = u8::decode(bytes)?;
+        Ok(Decoded {
+            value: Self::from(value),
+            next,
+        })
+    }
+}
+
+impl From<u8> for MessageType {
+    fn from(value: u8) -> Self {
+        match value {
+            1 => Self::Disconnect,
+            2 => Self::Ignore,
+            3 => Self::Unimplemented,
+            4 => Self::Debug,
+            5 => Self::ServiceRequest,
+            6 => Self::ServiceAccept,
+            20 => Self::KeyExchangeInit,
+            21 => Self::NewKeys,
+            value => Self::Unknown(value),
+        }
+    }
+}
+
+pub(crate) struct Packet<'a> {
+    pub(crate) payload: &'a [u8],
+}
+
+impl Packet<'_> {
+    pub(crate) fn encode(payload: &impl Encode, buf: &mut Vec<u8>) -> Result<(), Error> {
+        // <https://www.rfc-editor.org/rfc/rfc4253#section-6>
+        //
+        // Note that the length of the concatenation of 'packet_length',
+        // 'padding_length', 'payload', and 'random padding' MUST be a multiple
+        // of the cipher block size or 8, whichever is larger.  This constraint
+        // MUST be enforced, even when using stream ciphers.  Note that the
+        // 'packet_length' field is also encrypted, and processing it requires
+        // special care when sending or receiving packets.  Also note that the
+        // insertion of variable amounts of 'random padding' may help thwart
+        // traffic analysis.
+        //
+        // The minimum size of a packet is 16 (or the cipher block size,
+        // whichever is larger) bytes (plus 'mac').  Implementations SHOULD
+        // decrypt the length after receiving the first 8 (or cipher block size,
+        // whichever is larger) bytes of a packet.
+
+        let start = buf.len();
+        buf.extend_from_slice(&[0, 0, 0, 0]); // packet_length
+        buf.push(0); // padding_length
+
+        payload.encode(buf); // payload
+
+        let min_padding = 8 - (buf.len() - start) % 8;
+        let padding_len = match min_padding < 4 {
+            true => min_padding + 8,
+            false => min_padding,
+        };
+
+        buf[4] = padding_len as u8;
+
+        let padding_start = buf.len();
+        buf.extend(iter::repeat(0).take(padding_len)); // padding
+        if let Some(padding) = buf.get_mut(padding_start..) {
+            if rand::fill(padding).is_err() {
+                return Err(Error::Unreachable("failed to get random padding"));
+            }
+        }
+
+        buf.extend_from_slice(&[]); // mac
+
+        let packet_len = (buf.len() - start - 4) as u32;
+        buf[start..start + 4].copy_from_slice(&packet_len.to_be_bytes());
+
+        Ok(())
+    }
+}
+
+impl<'a> Decode<'a> for Packet<'a> {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error> {
+        let Decoded {
+            value: packet_length,
+            next,
+        } = PacketLength::decode(bytes)?;
+
+        let Decoded {
+            value: padding_length,
+            next,
+        } = PaddingLength::decode(next)?;
+
+        let payload_len = (packet_length.inner - 1 - padding_length.inner as u32) as usize;
+        let Some(payload) = next.get(..payload_len) else {
+            return Err(Error::Incomplete(Some(payload_len - next.len())));
+        };
+
+        let Some(next) = next.get(payload_len..) else {
+            return Err(Error::Unreachable(
+                "unable to extract rest after fixed-length slice",
+            ));
+        };
+
+        let Some(_) = next.get(..padding_length.inner as usize) else {
+            return Err(Error::Incomplete(Some(
+                padding_length.inner as usize - next.len(),
+            )));
+        };
+
+        // No MAC support yet
+
+        Ok(Decoded {
+            value: Self { payload },
+            next,
+        })
+    }
+}
+
+struct PacketLength {
+    inner: u32,
+}
+
+impl Decode<'_> for PacketLength {
+    fn decode(bytes: &[u8]) -> Result<Decoded<Self>, Error> {
+        let Decoded { value, next } = u32::decode(bytes)?;
+        if value > 256 * 1024 {
+            return Err(Error::InvalidPacket("packet too large"));
+        }
+
+        Ok(Decoded {
+            value: Self { inner: value },
+            next,
+        })
+    }
+}
+
+struct PaddingLength {
+    inner: u8,
+}
+
+impl Decode<'_> for PaddingLength {
+    fn decode(bytes: &[u8]) -> Result<Decoded<Self>, Error> {
+        let Decoded { value, next } = u8::decode(bytes)?;
+        if value < 4 {
+            return Err(Error::InvalidPacket("padding too short"));
+        }
+
+        Ok(Decoded {
+            value: Self { inner: value },
+            next,
+        })
+    }
+}
+
+impl Decode<'_> for u32 {
+    fn decode(bytes: &[u8]) -> Result<Decoded<Self>, Error> {
+        <[u8; 4]>::decode(bytes).map(|decoded| Decoded {
+            value: Self::from_be_bytes(decoded.value),
+            next: decoded.next,
+        })
+    }
+}
+
+impl<'a, const N: usize> Decode<'a> for [u8; N] {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error> {
+        let Some(inner) = bytes.get(..N) else {
+            return Err(Error::Incomplete(Some(N - bytes.len())));
+        };
+
+        let Some(next) = bytes.get(N..) else {
+            return Err(Error::Unreachable(
+                "unable to extract rest after fixed-length slice",
+            ));
+        };
+
+        let Ok(value) = <[u8; N]>::try_from(inner) else {
+            return Err(Error::Unreachable("fixed-length slice converts to array"));
+        };
+
+        Ok(Decoded { value, next })
+    }
+}
+
+impl<'a> Decode<'a> for u8 {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error> {
+        let Some(&inner) = bytes.first() else {
+            return Err(Error::Incomplete(Some(1)));
+        };
+
+        let Some(next) = bytes.get(1..) else {
+            return Err(Error::Unreachable("unable to extract rest after u8"));
+        };
+
+        Ok(Decoded { value: inner, next })
+    }
+}
+
+pub(crate) trait StreamState<'a> {
+    type Input: Encode;
+    type Output: Decode<'a>;
+
+    fn read(
+        &self,
+        stream: &'a mut TcpStream,
+        buf: &'a mut Vec<u8>,
+    ) -> impl Future<Output = Result<(Self::Output, usize), Error>> + 'a {
+        async move {
+            let len = stream.read_buf(buf).await?;
+            debug!(bytes = len, "read from stream");
+            let decoded = Self::Output::decode(buf)?;
+            Ok((decoded.value, decoded.next.len()))
+        }
+    }
+}
+
+pub(crate) trait Encode {
+    fn encode(&self, buf: &mut Vec<u8>);
+}
+
+pub(crate) trait Decode<'a>: Sized {
+    fn decode(bytes: &'a [u8]) -> Result<Decoded<'a, Self>, Error>;
+}
+
+pub(crate) struct Decoded<'a, T> {
+    pub(crate) value: T,
+    pub(crate) next: &'a [u8],
+}
