096f2a9e90c8424c904a47624dada6f8b89c657b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-08-30 15:12:55 +0200

    terminal: reinstate variable hint name

--- oxish/src/session/terminal.rs
+++ oxish/src/session/terminal.rs
@@ -148,7 +148,7 @@ impl Terminal {
             match result {
                 Ok(Err(e)) if e.raw_os_error() == Some(libc::EIO) => return Poll::Ready(Ok(0)),
                 Ok(result) => return Poll::Ready(result),
-                Err(_) => continue,
+                Err(_would_block) => continue,
             }
         }
     }
